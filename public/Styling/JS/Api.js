68fd218c1bbbcc67eac470c72c2dbbb50072b416
PawanKumar0209 <76720979+PawanKumar0209@users.noreply.github.com>
2022-10-31 12:09:22 +0530

    'ajax-call-success'

--- public/Styling/JS/Api.js
+++ public/Styling/JS/Api.js
@@ -1,36 +1,30 @@
 
 function ApiCall(arr,name){
     alert("called");
-    //console.log(arr);
     var data = Object.assign({}, arr)
     console.log(data);
-    
-    // var json_data = JSON.stringify(data); 
-    // console.log("json"+json_data);
-    
     $.ajax({
-    type:'POST',
-    url:'http://192.168.0.100:8074/Satrix_Saas/pub/register/index/index',
-   data: JSON.stringify(data),
-   enctype: 'multipart/form-data',
-   dataType : 'json',
-   processData: false,
-   contentType: false,
-   cors: false ,
-    headers : {
-      'Content-type' : 'application/json',
-      'Access-Control-Allow-Origin':'*',
-      'Access-Control-Allow-Headers':'Content-Type, X-Auth-Token, Authorization, Origin',
-      'token' : 'assss',
-      "Access-Control-Allow-Methods": [ "POST", "GET", "OPTIONS","DELETE","PUT"],
-      "X-Requested-With": "XMLHttpRequest"
-  },
-    cache:false,
-    crossDomain:true,
-    async:false,
-    success: function(data, status, xhr) {
-      console.log(data);
-      alert(data);
-    }
+      type:'POST',
+      dataType: 'json',
+      url:'http://192.168.0.100:8074/Satrix_Saas/pub/register/index/index',
+      headers : {
+          'Access-Control-Allow-Origin':'*',
+          'Access-Control-Allow-Headers':'Content-Type, X-Auth-Token, Authorization, Origin',
+          'token' : 'assss',
+          "Access-Control-Allow-Methods": [ "POST", "GET", "OPTIONS","DELETE","PUT"],
+          "X-Requested-With": "XMLHttpRequest"
+      },
+      data:{'data':JSON.stringify(data)},
+      success: function(result) {
+         var response = result.response;
+         if(response.success === 'true'){
+            alert(response.message);
+         }else{
+           alert(response.message);
+         }
+      },
+      error: function(result) {
+        alert("error");
+      }
   });
 }
