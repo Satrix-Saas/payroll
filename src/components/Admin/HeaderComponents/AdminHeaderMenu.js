3595d4f3a6cf43766f5ff9c68e0053161386ec21
Satrix-Saas <satrixsaas@gmail.com>
2022-11-02 19:11:12 +0530

    issues

--- src/components/Admin/HeaderComponents/AdminHeaderMenu.js
+++ src/components/Admin/HeaderComponents/AdminHeaderMenu.js
@@ -4,31 +4,31 @@ import { Link } from 'react-router-dom'
 export default function AdminHeaderMenu() {
   return (
     <ul className="header-new-menu " id="headerNewMenu">
-					<li className='headerli'>
+					<li >
 						<Link to="/" data-bs-toggle="dropdown" id="dropmain" role="button" aria-haspopup="true" aria-expanded="false">Clients</Link>
-						<div className="dropdown-menu" >
-							<Link className="dropdown-item" id="dropitem" to="clients.html">Clients</Link>
-						</div>
+						<ul className="dropdown-menu" >
+							<li><Link className="dropdown-item" id="dropitem" to="clients.html">Clients</Link></li>
+						</ul>
 					</li>
-					<li className='headerli'>
+					<li >
 						<Link to="/" data-bs-toggle="dropdown" id="dropmain" role="button" aria-haspopup="true" aria-expanded="false">Projects</Link>
-						<div className="dropdown-menu">
-							<Link className="dropdown-item" id="dropitem" to="projects.html">Projects</Link>
-							<Link className="dropdown-item" id="dropitem" to="tasks.html">Tasks</Link>
-							<Link className="dropdown-item" id="dropitem" to="task-board.html">Task Board</Link>
-						</div>
+						<ul className="dropdown-menu">
+							<li><Link className="dropdown-item" id="dropitem" to="projects.html">Projects</Link></li>
+							<li><Link className="dropdown-item" id="dropitem" to="tasks.html">Tasks</Link></li>
+							<li><Link className="dropdown-item" id="dropitem" to="task-board.html">Task Board</Link></li>
+						</ul>
 					</li>
-					<li className='headerli'>
+					<li >
 						<Link to="/" data-bs-toggle="dropdown" id="dropmain" role="button" aria-haspopup="true" aria-expanded="false">Leads</Link>
-						<div className="dropdown-menu">
-							<Link className="dropdown-item" id="dropitem" to="leads.html">Leads</Link>
-						</div>
+						<ul className="dropdown-menu">
+						<li><Link className="dropdown-item" id="dropitem" to="leads.html">Leads</Link></li>
+						</ul>
 					</li>
-					<li className='headerli'>
+					<li >
 						<Link to="/" data-bs-toggle="dropdown" id="dropmain" role="button" aria-haspopup="true" aria-expanded="false">Tickets</Link>
-						<div className="dropdown-menu" >
-							<Link className="dropdown-item" id="dropitem" to="tickets.html">Tickets</Link>
-						</div>
+						<ul className="dropdown-menu" >
+						<li><Link className="dropdown-item" id="dropitem" to="tickets.html">Tickets</Link></li>
+						</ul>
 					</li>
 				</ul>
 
